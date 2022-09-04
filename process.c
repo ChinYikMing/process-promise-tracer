5772fad925bfd712241409a412e71b8bb68f1c3a
Crazyremi <890906remi@gmail.com>
2022-09-04 03:12:59 -0700

    add close & munmap to process.c

--- process.c
+++ process.c
@@ -14,7 +14,9 @@ bool process_promise_pass(Process *proc){
 	int fd = open(proc->exe, O_RDONLY);
 	int filesize = lseek(fd,0,SEEK_END);
 	uint8_t* data = mmap(NULL, filesize, PROT_READ, MAP_SHARED, fd, 0);
-
+	close(fd);
+	munmap(data, filesize);
+	
 	elf = (Elf64_Ehdr *) data;
 	shdr = (Elf64_Shdr *) (data + elf->e_shoff);
 	char* strtab = (char *)(data + shdr[elf->e_shstrndx].sh_offset);
@@ -31,7 +33,7 @@ bool process_promise_pass(Process *proc){
 		    fprintf(fp,"%c", data[k]);
 		}   
 		fclose(fp);
-    	}
+    }
 	JsonParser *parser = json_parser_new();
 	JsonNode *node = json_node_new(JSON_NODE_OBJECT);
 	json_parser_load_from_file(parser, "file.json", NULL);
