424e114b8d800fa39a20918e0b1319cf5cb36cc5
ChinYikMing <yikming2222@gmail.com>
2022-11-06 19:54:49 +0800

    add send_notification func

--- signal.c
+++ signal.c
@@ -6,6 +6,27 @@ void sighup_handler(int signum){
 	sighup_coming = 1;
 }
 
-int send_signal(pid_t pid, int signum){
-	return kill(pid, signum);
+void send_notification(Process *proc, const char *notification){
+	char pid[32] = {0};
+        sprintf(pid, "%d", proc->pid);
+
+        char fd0_path[32] = {0};
+        strcpy(fd0_path, PROC_DIR);
+        strcat(fd0_path, "/");
+        strcat(fd0_path, pid);
+        strcat(fd0_path, "/");
+        strcat(fd0_path, "fd");
+        strcat(fd0_path, "/");
+        strcat(fd0_path, "0");
+
+	int fd0;
+	fd0 = open(fd0_path, O_WRONLY);
+	write(fd0, notification, strlen(notification));
+	close(fd0);
+}
+
+int send_signal(Process *proc, int signum, const char *notification){
+	if(notification)
+		send_notification(proc, notification);
+	return kill(proc->pid, signum);
 }
