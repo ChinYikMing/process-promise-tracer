94b40a5af683d6dbc97454ad724372aa1014c5b6
s55669874 <s5566987457@gmail.com>
2022-11-17 15:42:52 +0800

    config

--- config.c
+++ config.c
@@ -31,10 +31,12 @@ int config_read(Config *cf, const char *config_file){
 			mark=1;
 			continue;
 		}
-		else if(strcmp(title2, buf)){
+		else{break;}
+		if(strcmp(title2, buf)){
 			mark=2;
 			continue;
 		}
+		else{break;}
 		if(mark == 1){
 			char *type, *val;
 			type = strtok(buf, "=");
@@ -42,7 +44,7 @@ int config_read(Config *cf, const char *config_file){
 			c = conf_create(type, val);
 			config_add(cf, c);
 		}
-		if(mark == 2){
+		else if(mark == 2){
 			char *val;
 			val = buf;
 			c = conf_create(CONF_TYPE_PROG, val);
