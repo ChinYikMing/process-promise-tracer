d33e992bde4ec5d69b35af31ae452beb0afd455c
ChinYikMing <yikming2222@gmail.com>
2022-11-21 23:26:48 +0800

    refactor config read

--- config.c
+++ config.c
@@ -24,19 +24,59 @@ int config_parse(const char *config_file){
 
 int config_read(Config *cf){
 	FILE *config_fptr = fopen(CONFIG_FILE, "r");
-	if(!config_fptr)
-		return 1;
+        if(!config_fptr)
+                return 1;
 
-	char buf[BUF_SIZE] = {0};
-	Conf *c = NULL;
-	while(fgets(buf, BUF_SIZE, config_fptr)){
-		buf[strcspn(buf, "\r\n")] = 0;
-		c = conf_create(CONF_TYPE_PROG, buf);
-		config_add(cf, c);
-	}
+        char buf[BUF_SIZE] = {0};
+        Conf *c = NULL;
+        int mark = 0;
+        char title1[] = "[Daemon]";
+        char title2[] = "[Untrusted Program]";
+        size_t buf_len;
 
-	fclose(config_fptr);
-	return 0;
+        while(fgets(buf, BUF_SIZE, config_fptr)){
+                buf[strcspn(buf, "\r\n")] = 0;
+                buf_len = strlen(buf);
+                if(!buf_len)
+                        continue;
+
+                if(0 == strcmp(title1, buf)){
+                        mark=1;
+                        continue;
+                }
+                else if(0 == strcmp(title2, buf)){
+                        mark=2;
+                        continue;
+                }
+                else{
+                        if(mark == 1){
+                                char *type, *val;
+                                type = strtok(buf, "=");
+                                val = strtok(NULL, "=");
+                                c = conf_create(type, val);
+                                config_add(cf, c);
+                        }
+                        else if(mark == 2){
+                                char *val;
+                                val = buf;
+                                c = conf_create(CONF_TYPE_PROG, val);
+                                config_add(cf, c);
+                        }
+                }
+        }
+
+	/* debug print
+        Node *iter;
+        Conf *conf;
+        LIST_FOR_EACH(cf->list, iter){
+                conf = LIST_ENTRY(iter, Conf);
+                printf("key: %s, val: %s\n", conf->key, conf->val);
+        }
+        exit(1);
+	*/
+
+        fclose(config_fptr);
+        return 0;
 }
 
 int config_add(Config *cf, Conf *c){
