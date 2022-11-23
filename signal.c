e6477426c27b310708acac0409dbd8d0adbf91bd
ChinYikMing <yikming2222@gmail.com>
2022-11-23 20:01:36 +0800

    refactor send_signal function

--- signal.c
+++ signal.c
@@ -6,17 +6,24 @@ void sighup_handler(int signum){
 	sighup_coming = 1;
 }
 
-void send_notification(Process *proc, const char *notification){
-	int tty_fd;
-	tty_fd = open(proc->tty_path, O_WRONLY);
-	if(-1 == tty_fd)
+void send_notification(Process *proc, int signum, const char *notification){
+	FILE *tty_ptr = fopen(proc->tty_path, "w");
+	if(!tty_ptr)
 		return;
-	write(tty_fd, notification, strlen(notification));
-	close(tty_fd);
+
+	fprintf(tty_ptr, "%s\n", notification);
+
+	if(SIGSTOP == signum){
+		fprintf(tty_ptr, "To resume the process, you could use 'jobs' command to check the jobID then use 'fg %%jobID' command\n");
+		fprintf(tty_ptr, "To kill the process, you could use 'kill -s 9 %d' command\n", proc->pid);
+	}
+
+	fclose(tty_ptr);
 }
 
 int send_signal(Process *proc, int signum, const char *notification){
 	if(notification)
-		send_notification(proc, notification);
+		send_notification(proc, signum, notification);
+
 	return kill(proc->pid, signum);
 }
